182573fbedac76c786cb2d986478e8bcf719b0cb
Xc-md <53468508+Xc-md@users.noreply.github.com>
2021-08-29 16:50:44 +0800

    Update flag.py

--- Day01-15/code/Day01/flag.py
+++ Day01-15/code/Day01/flag.py
@@ -49,21 +49,22 @@ def main():
     width, height = 540, 360
     draw_rectangle(x, y, width, height)
     # 画大星星
-    pice = 22
-    center_x, center_y = x + 5 * pice, y + height - pice * 5
+    widthDivisor=30
+    pace = width/widthDivisor
+    center_x, center_y = x + 5 * pace, y + height - pace * 5
     turtle.goto(center_x, center_y)
     turtle.left(90)
-    turtle.forward(pice * 3)
+    turtle.forward(pace * 3)
     turtle.right(90)
-    draw_star(turtle.xcor(), turtle.ycor(), pice * 3)
+    draw_star(turtle.xcor(), turtle.ycor(), pace * 3)
     x_poses, y_poses = [10, 12, 12, 10], [2, 4, 7, 9]
     # 画小星星
     for x_pos, y_pos in zip(x_poses, y_poses):
-        turtle.goto(x + x_pos * pice, y + height - y_pos * pice)
+        turtle.goto(x + x_pos * pace, y + height - y_pos * pace)
         turtle.left(turtle.towards(center_x, center_y) - turtle.heading())
-        turtle.forward(pice)
+        turtle.forward(pace)
         turtle.right(90)
-        draw_star(turtle.xcor(), turtle.ycor(), pice)
+        draw_star(turtle.xcor(), turtle.ycor(), pace)
     # 隐藏海龟
     turtle.ht()
     # 显示绘图窗口
